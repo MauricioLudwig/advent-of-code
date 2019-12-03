5ecfce9ac83c5dadbc5416c336e68fcde515c57e
Mauricio Ludwig <mrdamiengallagher@gmail.com>
2019-12-03 18:40:42 +0100

    day 1, part 2

--- advent/day-1/day-1.js
+++ advent/day-1/day-1.js
@@ -1,17 +1,43 @@
 const fs = require('fs');
 const path = require('path');
 
-const fileName = process.env.MODE === 'test'
-    ? 'numbers-test.txt'
-    : 'numbers.txt';
+const getNumbers = () => {
+    const fileName = process.env.MODE === 'test'
+        ? 'numbers-test.txt'
+        : 'numbers.txt';
 
-const numbers = fs
-    .readFileSync(path.resolve(__dirname, fileName))
-    .toString()
-    .split('\n');
+    return fs
+        .readFileSync(path.resolve(__dirname, fileName))
+        .toString()
+        .split('\n');
+};
 
-const fuelRequirement = numbers.reduce((acc, num) => {
-    return acc + (Math.floor(num / 3) - 2);
-}, 0);
+const partOne = () => {
+    const numbers = getNumbers();
 
-module.exports = fuelRequirement;
+    return numbers.reduce((acc, num) => {
+        return acc + (Math.floor(num / 3) - 2);
+    }, 0);
+};
+
+const partTwo = () => {
+    const numbers = getNumbers();
+
+    return numbers.reduce((acc, num) => {
+        let fuelSum = 0;
+        let fuel = num;
+        while (true) {
+            fuel = Math.floor(fuel / 3) - 2;
+            if (fuel < 1) {
+                break;
+            }
+            fuelSum += fuel;
+        }
+        return acc + fuelSum;
+    }, 0);
+};
+
+module.exports = {
+    partOne,
+    partTwo
+};
--- advent/day-1/day-1.test.js
+++ advent/day-1/day-1.test.js
@@ -1,5 +1,9 @@
-const fuelRequirement = require('./day-1');
+const { partOne, partTwo } = require('./day-1');
 
-test('Should correctly calculate sum of fuel for numbers test file', () => {
-    expect(fuelRequirement).toEqual(34273);
+test('Should correctly calculate sum of fuel for part one', () => {
+    expect(partOne()).toEqual(34241);
+});
+
+test('Should correctly calculate sum of fuel for part two', () => {
+    expect(partTwo()).toEqual(51316);
 });
