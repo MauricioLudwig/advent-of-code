ac19d7289ceb7bbe5c251c863fefd42edf1186ba
Mauricio Ludwig <mrdamiengallagher@gmail.com>
2020-01-04 09:36:52 +0100

    day 16, part 2

--- advent/day-16/day-16.js
+++ advent/day-16/day-16.js
@@ -2,11 +2,12 @@ const {
     puzzleInput
 } = require('./input');
 
-const day16 = () => {
+const partOne = () => {
+    const phases = 100;
     const pattern = [0, 1, 0, -1];
     let input = puzzleInput.split('');
 
-    for (let i = 0; i < 100; i++) {
+    for (let i = 0; i < phases; i++) {
         let newInput = [];
         for (let y = 1, len = input.length; y <= len; y++) {
             let repetition = y;
@@ -29,10 +30,39 @@ const day16 = () => {
         input = [...newInput];
     }
 
-    console.log('first eight digits', input.slice(0, 8).join(''));
-    console.log('Program finished');
+    console.log('Program finished (part 1): ', input.slice(0, 8).join(''));
 };
 
 const getLastDigit = number => number.toString().slice(-1);
 
-module.exports = day16;
+const partTwo = () => {
+    const phases = 100;
+    const inputRepetition = 10000;
+    let input = '';
+
+    for (let i = 0; i < inputRepetition; i++) {
+        input += puzzleInput;
+    }
+
+    const messageOffset = parseInt(input.slice(0, 7));
+
+    for (let i = 0; i < phases; i++) {
+        let newInput = '';
+        let acc = 0;
+
+        for (let y = input.length - 1; y >= messageOffset; y--) {
+            const digit = parseInt(input.charAt(y));
+            newInput = ((digit + acc) % 10) + newInput;
+            acc += digit;
+        }
+
+        input = input.slice(0, messageOffset) + newInput;
+    }
+
+    console.log('Program finished (part 2):', input.slice(messageOffset, messageOffset + 8));
+};
+
+module.exports = {
+    partOne,
+    partTwo
+};
