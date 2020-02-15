639fc870e2dfa625c9a9f4b0a14fa2e5747c2746
Mauricio Ludwig <mrdamiengallagher@gmail.com>
2020-02-15 14:29:11 +0100

    day 24

--- advent/day-24/day-24.js
+++ advent/day-24/day-24.js
@@ -1,3 +1,4 @@
+const { done, success } = require('../utils/console-logger');
 const fs = require('fs');
 const path = require('path');
 
@@ -36,7 +37,7 @@ const partOne = () => {
     input = JSON.parse(JSON.stringify(newInput));
   }
 
-  console.log('Program finished');
+  done('Program finished', 'Part 1');
 };
 
 const checkAdjacentTiles = (arr, i, y, gridSize) => {
@@ -64,6 +65,128 @@ const calculateBioDiversityRating = (layout) => layout.split('').reduce((acc, cu
   return acc;
 }, 0);
 
+const partTwo = () => {
+  const origin = 0;
+  const levels = 200;
+  const minutes = 200;
+
+  let foldedSpace = {
+    [origin]: getPuzzleInput()
+  };
+
+  for (let i = 1; i <= levels; i++) {
+    foldedSpace[origin + i] = getEmptyMatrix(); // LEVEL ABOVE (+1)
+    foldedSpace[origin - i] = getEmptyMatrix(); // LEVEL BELOW (-1)
+  }
+
+  for (let i = 0; i < minutes; i++) {
+    const cachedFoldedSpace = JSON.parse(JSON.stringify(foldedSpace));
+    for (let i = -Math.abs(levels); i <= levels; i++) {
+      calculateBugGrowth(foldedSpace, cachedFoldedSpace, i);
+    }
+  }
+
+  success(`Number of bugs present after ${minutes} minutes`, 'Part 2', calculateNumberOfBugs(foldedSpace));
+  done('Program finished', 'Part 2');
+};
+
+const getEmptyMatrix = () => {
+  let grid = [];
+  const gridSize = 5;
+
+  for (let i = 0; i < gridSize; i++) {
+    let row = [];
+    for (let y = 0; y < gridSize; y++) {
+      row.push('.');
+    }
+    grid.push(row);
+  }
+
+  return grid;
+};
+
+const calculateNumberOfBugs = space => Object
+  .values(space)
+  .flat(Infinity)
+  .filter(o => o === '#')
+  .length;
+
+const calculateBugGrowth = (space, cachedSpace, index) => {
+  const gridSize = 5;
+  const plane = space[index];
+  const cachedPlane = cachedSpace[index];
+  const cachedPlaneUp = cachedSpace[index + 1];
+  const cachedPlaneDown = cachedSpace[index - 1];
+
+  for (let i = 0; i < gridSize; i++) {
+    for (let y = 0; y < gridSize; y++) {
+      if (i === 2 && y === 2) {
+        continue;
+      }
+
+      let tiles = [];
+
+      const tile8 = cachedPlaneUp ? cachedPlaneUp[1][2] : '.';
+      const tile12 = cachedPlaneUp ? cachedPlaneUp[2][1] : '.';
+      const tile14 = cachedPlaneUp ? cachedPlaneUp[2][3] : '.';
+      const tile18 = cachedPlaneUp ? cachedPlaneUp[3][2] : '.';
+
+      const tileAE = cachedPlaneDown ? getRowValues(cachedPlaneDown, 0) : '.';
+      const tileAU = cachedPlaneDown ? getColumnValues(cachedPlaneDown, 0) : '.';
+      const tileEY = cachedPlaneDown ? getColumnValues(cachedPlaneDown, gridSize - 1) : '.';
+      const tileUY = cachedPlaneDown ? getRowValues(cachedPlaneDown, gridSize - 1) : '.';
+
+      // UP
+      if (i === 0) {
+        tiles.push(tile8)
+      } else if (i === 3 && y === 2) {
+        tiles.push(tileUY);
+      } else {
+        tiles.push(cachedPlane[i - 1][y]);
+      }
+
+      // LEFT
+      if (y === 0) {
+        tiles.push(tile12);
+      } else if (i === 2 && y === 3) {
+        tiles.push(tileEY);
+      } else {
+        tiles.push(cachedPlane[i][y - 1]);
+      }
+
+      // RIGHT
+      if (y === gridSize - 1) {
+        tiles.push(tile14);
+      } else if (i === 2 && y === 1) {
+        tiles.push(tileAU);
+      } else {
+        tiles.push(cachedPlane[i][y + 1]);
+      }
+
+      // DOWN
+      if (i === gridSize - 1) {
+        tiles.push(tile18);
+      } else if (i === 1 && y === 2) {
+        tiles.push(tileAE);
+      } else {
+        tiles.push(cachedPlane[i + 1][y]);
+      }
+
+      const nrOfBugs = tiles.flat(Infinity).filter(o => o === '#').length;
+
+      if (cachedPlane[i][y] === '.' && (nrOfBugs === 1 || nrOfBugs === 2)) {
+        plane[i][y] = '#';
+      } else if (cachedPlane[i][y] === '#' && nrOfBugs !== 1) {
+        plane[i][y] = '.';
+      }
+    }
+  }
+};
+
+const getRowValues = (arr, n) => arr[n];
+const getColumnValues = (arr, n) => arr.map(o => o[n]);
+
 module.exports = {
-  partOne
+  partOne,
+  partTwo
 };
