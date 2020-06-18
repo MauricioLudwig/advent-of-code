9db8c7b36716d56dc1f96dd9ee1b9d1e21516397
Mauricio Ludwig <mrdamiengallagher@gmail.com>
2020-06-18 19:46:28 +0200

    2018, day 12

--- 2018/days/12.js
+++ 2018/days/12.js
@@ -1,14 +1,14 @@
 import chalk from 'chalk';
 import { getAsArray } from '../input/index.js';
-import { end } from '../utils/logger.js';
+import { end, success } from '../utils/logger.js';
 
 export default () => {
   const [line1, , ...input] = getAsArray('12.txt');
   const [, initialState] = line1.match(/initial state: ([\.|#]+$)/);
 
-  const offsetLeft = 2;
+  const offsetLeft = 5;
   const offsetRight = 50;
-  const numOfGenerations = 40;
+  const numOfGenerations = 1000;
 
   const pots = initialState.split('');
 
@@ -25,9 +25,15 @@ export default () => {
     return { condition, product };
   });
 
-  for (let i = 0; i < numOfGenerations; i++) {
+  for (let i = 0; i <= numOfGenerations; i++) {
     const currentGeneration = [...pots];
-    console.log(chalk.magenta(`Generation: ${i < 10 ? '0' : ''}${i} (${chalk.yellow(getPotsSum(pots, offsetLeft))})`), pots.join(''));
+
+    if (i === 20) {
+      success(`Part 1: ${getPotsSum(pots, offsetLeft)}`);
+    }
+
+    // Uncomment line below to print generation growth over time *
+    // console.log((`Generation: ${chalk.blueBright(i)} (${chalk.yellow(getPotsSum(pots, offsetLeft))})`));
 
     for (let y = 0; y < currentGeneration.length; y++) {
       const LL = getPot(currentGeneration, y - 2);
@@ -40,6 +46,15 @@ export default () => {
       pots[y] = newPot ? newPot.product : '.';
     }
   }
+
+  /*
+    Part 2 solution derived from looking at the printout (*) of each generation of plants. For each generation (given an adequately high integer) each subsequent
+    generation increases by 5, always.
+
+    Taking the base value from a point in time (generation = 1000) we can immediately calculate the predicted sum after fifty billion (50000000000) generations.
+  */
+  const sum = ((50000000000 - 1000) * 5) + 5219;
+  success(`Part 2: ${sum}`);
 
   end();
 };
