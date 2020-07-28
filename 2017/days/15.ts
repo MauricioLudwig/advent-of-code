9d2c33b0a48fe5e9d9d67015deb7dd7da3fbea0b
Mauricio Ludwig <mrdamiengallagher@gmail.com>
2020-07-28 19:24:17 +0200

    day 15 (2017)

--- 2017/days/15.ts
+++ 2017/days/15.ts
@@ -1,37 +1,84 @@
-import { success, end } from '../utils/logger';
+import { performance } from 'perf_hooks';
+import { success, end, logPerformance } from '../utils/logger';
+
+/*
+  -- Reflection
+
+  Using toString(2) to convert string to binary, then comparing the last 16 bits
+  yielded a correct answer but with an execution time of 57.87 seconds (Part 1).
+
+  Improving upon the previous solution, I instead opt for the bitwise AND operation, on each value, against
+  the string 0xFFFF, whose binary representation is 16 1's. On a bit level we can then compare the two values.
+  This approach yields a correct answer but with a significantly improved execution time of 0.77 seconds.
+
+  The aforementioned approach yields an execution time of 1.88 seconds for Part 2.
+*/
 
 class Generator {
   divisor = 2147483647;
 
-  constructor(public value: number, public factor: number) {}
+  constructor(
+    public value: number,
+    public factor: number,
+    public multiple: number
+  ) {}
 
   generateNextValue() {
     this.value = (this.value * this.factor) % this.divisor;
+    return this.value;
   }
 
-  get binary() {
-    return this.value.toString(2);
+  generateNextMultipleValue() {
+    while (true) {
+      this.generateNextValue();
+
+      if (this.value % this.multiple === 0) {
+        break;
+      }
+    }
+
+    return this.value;
   }
 }
 
 export default (): void => {
-  const genA = new Generator(289, 16807);
-  const genB = new Generator(629, 48271);
+  const genA1 = new Generator(65, 16807, 4);
+  const genB1 = new Generator(8921, 48271, 8);
 
-  let judgeCount = 0;
+  let judgeCount1 = 0;
+  const t1 = performance.now();
 
   for (let i = 0; i < 40e6; i++) {
-    genA.generateNextValue();
-    genB.generateNextValue();
+    const a = genA1.generateNextValue();
+    const b = genB1.generateNextValue();
 
-    const binA = genA.binary;
-    const binB = genB.binary;
-
-    if (binA.substring(binA.length - 16) === binB.substring(binB.length - 16)) {
-      judgeCount++;
+    if ((a & 0xffff) === (b & 0xffff)) {
+      judgeCount1++;
     }
   }
 
-  success(`Part 1: ${judgeCount}`);
+  const t2 = performance.now();
+  logPerformance(t2, t1);
+  success(`Part 1: ${judgeCount1}`);
+
+  const genA2 = new Generator(289, 16807, 4);
+  const genB2 = new Generator(629, 48271, 8);
+
+  let judgeCount2 = 0;
+  const t3 = performance.now();
+
+  for (let i = 0; i < 5e6; i++) {
+    const a = genA2.generateNextMultipleValue();
+    const b = genB2.generateNextMultipleValue();
+
+    if ((a & 0xffff) === (b & 0xffff)) {
+      judgeCount2++;
+    }
+  }
+
+  const t4 = performance.now();
+  logPerformance(t4, t3);
+  success(`Part 2: ${judgeCount2}`);
+
   end();
 };
