788b1eaf18558cec4a7f0c7a3fa3d6ce97ae43cc
Mauricio Ludwig <mrdamiengallagher@gmail.com>
2020-07-31 06:31:54 +0200

    day 7 (2017)

--- 2017/days/07.ts
+++ 2017/days/07.ts
@@ -1,39 +1,71 @@
 import { getAsArray } from '../input';
 import { success, end } from '../utils/logger';
 
-interface Input {
-  origin: string;
-  number: number;
-  next: string[];
+interface Tower {
+  name: string;
+  weight: number;
+  discs: string[];
 }
 
 export default (): void => {
-  const input = getAsArray('07.txt').map(
-    (o): Input => {
+  const input = getAsArray('07.txt');
+  const towers = input.map(
+    (o): Tower => {
       const [left, right] = o.split('->');
-      const [, origin, number] = left.match(/(\w+) \((\d+)\)/) ?? [];
-      const next = right?.split(',').map((o): string => o.trim()) ?? [];
+      const [, name, weight] = left.match(/(\w+) \((\d+)\)/) ?? [];
+      const discs = right?.split(',').map((o): string => o.trim()) ?? [];
 
       return {
-        origin,
-        number: parseInt(number, 10),
-        next,
+        name,
+        weight: parseInt(weight, 10),
+        discs,
       };
     }
   );
 
-  const originCollection = input.map((o): string => o.origin);
-
-  const nextCollection = input
-    .map((o): string[] => o.next)
-    .filter((o): boolean => o.length > 0)
-    .flat();
-
-  const [origin] = originCollection.filter(
-    (o): boolean => !nextCollection.some((x): boolean => o === x)
+  const [origin] = towers.filter(
+    (o) => !towers.some((x) => x.discs.includes(o.name))
   );
 
-  success(`Part 1: ${origin}`);
-
+  success(`Part 1: ${origin.name}`);
+  calcTowerWeight(origin.name, towers); // Part 2
   end();
+};
+
+type CalcTowerWeight = (name: string, towers: Tower[]) => number;
+
+const calcTowerWeight: CalcTowerWeight = (name, towers) => {
+  const tower = towers.find((o) => o.name === name);
+
+  if (!tower) {
+    return 0;
+  }
+
+  const subTowers = tower.discs.map((disc): {
+    name: string;
+    totalWeight: number;
+  } => ({
+    name: disc,
+    totalWeight: calcTowerWeight(disc, towers),
+  }));
+
+  if (!subTowers.every((o) => o.totalWeight === subTowers[0].totalWeight)) {
+    const [subTower] = subTowers.filter((o, oi) => {
+      return !subTowers.some(
+        (x, xi) => oi !== xi && o.totalWeight === x.totalWeight
+      );
+    });
+
+    const [n1, n2] = Array.from(
+      new Set(subTowers.map((o): number => o.totalWeight))
+    );
+
+    const subTowerWeight =
+      towers.find((o) => o.name === subTower.name)?.weight ?? 0;
+    success(`Part 2: ${subTowerWeight - Math.abs(n1 - n2)}`); // only applicable for first instance
+  }
+
+  return (
+    tower.weight + subTowers.reduce((acc, curr) => acc + curr.totalWeight, 0)
+  );
 };
