7d447237219e390b8f7aea67fbd70bb5cd82a310
Mauricio Ludwig <mrdamiengallagher@gmail.com>
2020-08-11 08:31:08 +0200

    update app.ts

--- 2017/app.ts
+++ 2017/app.ts
@@ -1,1 +1,14 @@
+/*
+
+  In order to use:
+  1. import (default) function from './days/XX' where XX represents the day
+  2. invoke the imported function
+  
+  E.g.
+
+  import myFunc from './days/01';
+  myFunc();
+
+*/
+
 console.log('app.ts running!');
