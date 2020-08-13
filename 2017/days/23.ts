790b8e20cb68257d0c2d742d288166dd20c3731b
Mauricio Ludwig <mrdamiengallagher@gmail.com>
2020-08-13 07:10:23 +0200

    day 23 (2017)

--- 2017/days/23.ts
+++ 2017/days/23.ts
@@ -2,7 +2,7 @@ import { getAsArray } from '../input';
 import { success, end } from '../utils/logger';
 import { IInstruction, IRegister, Operation, Program } from './18';
 
-export default (): void => {
+export default async (): Promise<void> => {
   const instructions: IInstruction[] = getAsArray('23.txt').map(
     (o): IInstruction => {
       const [operation, x, y] = o.split(' ');
@@ -21,16 +21,43 @@ export default (): void => {
     };
   }, {});
 
-  const program = new Program({ ...register });
+  const programA = new Program({ ...register });
 
   while (true) {
-    program.run2(instructions);
+    programA.run2(instructions);
 
-    if (program.index > instructions.length - 1) {
+    if (programA.index > instructions.length - 1) {
       break;
     }
   }
 
-  success(`Part 1: ${program.operationsInvoked[Operation.mul]}`);
+  success(`Part 1: ${programA.operationsInvoked[Operation.mul]}`);
+
+  let h = 0;
+  let b = 67 * 100 + 100000;
+  let c = b + 17000;
+  let offset = 17;
+
+  for (let i = b; i <= c; i += offset) {
+    let d = 2; // skip 1 since a prime number is always divisible by 1
+    let isPrime = true;
+
+    // skip d === i since a prime number is always divisible by itself
+    while (d < i) {
+      if (i % d === 0) {
+        isPrime = false;
+        break;
+      }
+
+      d++;
+    }
+
+    if (!isPrime) {
+      h++;
+    }
+  }
+
+  success(`Part 2: ${h}`);
+
   end();
 };
