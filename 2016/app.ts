abfc6e9c2ed9da8667bd5048d97ee575395ecd1e
Mauricio Ludwig <mrdamiengallagher@gmail.com>
2020-09-30 02:34:08 +0200

    update

--- 2016/app.ts
+++ 2016/app.ts
@@ -1,1 +1,17 @@
-console.log('app.ts is running!');
+/*
+
+  In order to use:
+  1. import (default) function from './days/XX' where XX represents the day
+  2. invoke the imported function
+  
+  E.g.
+
+  import myFunc from './days/01';
+  myFunc();
+
+*/
+
+import x from './days/01';
+x();
+
+console.log('app.ts running!');
