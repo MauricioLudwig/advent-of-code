565b7d9bf52c99f95c62fc71f9168cf3455dd9a3
Mauricio Ludwig <mrdamiengallagher@gmail.com>
2020-10-07 20:27:34 +0200

    day 1, part 2 (2015)

--- 2015/Days/01.cs
+++ 2015/Days/01.cs
@@ -9,8 +9,24 @@ namespace _2015.Days
         public void Run()
         {
             var instructions = Helper.GetAsSingleLine("01.txt");
+            Console.WriteLine(instructions.Length);
             var floor = instructions.Aggregate(0, (acc, curr) => acc + (curr == '(' ? 1 : -1));
             Console.WriteLine(floor);
+
+            var index = 0;
+            var currentFloor = 0;
+
+            foreach (var instruction in instructions)
+            {
+                index++;
+                currentFloor += instruction == '(' ? 1 : -1;
+                if (currentFloor == -1)
+                {
+                    break;
+                }
+            }
+
+            Console.WriteLine(index);
         }
     }
 }
