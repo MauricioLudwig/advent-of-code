ae4eb6f1254ab9fe04322ac3b96ffc0a993e990e
Mauricio Ludwig <mrdamiengallagher@gmail.com>
2020-12-06 08:39:16 +0100

    remove custom prettier config

--- 2020/days/06.ts
+++ 2020/days/06.ts
@@ -1,23 +1,30 @@
-import { getAsMatrix, Divisor } from "../input";
-import { success } from "../utils/logger";
-import { arrangeByOccurrence } from "../utils/array-methods";
+import { getAsMatrix, Divisor } from '../input';
+import { success } from '../utils/logger';
+import { arrangeByOccurrence } from '../utils/array-methods';
 
 export default (): void => {
-  const input = getAsMatrix("06.txt", Divisor.NewLine);
+  const input = getAsMatrix('06.txt', Divisor.NewLine);
 
   (() => {
-    const count = input.reduce((acc, curr) => acc + new Set(Array.from(collapseArr(curr))).size, 0);
+    const count = input.reduce(
+      (acc, curr) => acc + new Set(Array.from(collapseArr(curr))).size,
+      0
+    );
     success(`Part 1: ${count}`);
   })();
 
   (() => {
     const count = input.reduce(
       (acc, curr) =>
-        acc + Object.values(arrangeByOccurrence(collapseArr(curr))).filter((o) => o === curr.length).length,
+        acc +
+        Object.values(arrangeByOccurrence(collapseArr(curr))).filter(
+          (o) => o === curr.length
+        ).length,
       0
     );
     success(`Part 2: ${count}`);
   })();
 };
 
-const collapseArr = (arr: string[]): string[] => arr.map((o) => o.split("")).flat();
+const collapseArr = (arr: string[]): string[] =>
+  arr.map((o) => o.split('')).flat();
