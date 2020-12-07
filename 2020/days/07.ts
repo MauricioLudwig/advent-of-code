97bd8ab7ccba833b590dca6e2fcebecc86e638c7
Mauricio Ludwig <mrdamiengallagher@gmail.com>
2020-12-07 08:19:43 +0100

    refactor solution

--- 2020/days/07.ts
+++ 2020/days/07.ts
@@ -6,11 +6,13 @@ type Bag = {
   innerBags: Record<string, number> | null;
 };
 
+const SHINY_GOLD = 'shiny gold';
+
 export default () => {
   const input: Array<Bag> = getAsArray('07.txt').map((o) => {
     const [, outerBag] = o.match(/([a-z ]+) bags? contain/) || [];
-    const inner = o.split(',');
-    if (inner.join('').includes('no other bags')) {
+
+    if (o.split(',').join('').includes('no other bags')) {
       return {
         outerBag,
         innerBags: null,
@@ -19,44 +21,29 @@ export default () => {
 
     const innerBags = o.split(',').reduce((acc, curr) => {
       const [, amount, content] = curr.match(/(\d+) ([a-z ]+) bags?/) || [];
-
       return {
         ...acc,
         [content]: parseInt(amount, 10),
       };
     }, {} as Record<string, number>);
+
     return { outerBag, innerBags };
   });
 
-  // console.log('input', input);
-
-  const bagColors = new Set<string>();
-
   const bagsContainingGoldenBag = input.reduce((acc, curr) => {
-    if (!curr.innerBags) {
-      return acc;
-    }
-
     const hasGoldenBag = containsGoldenBag(curr.outerBag, input);
-
-    if (hasGoldenBag && !curr.outerBag.includes('shiny gold')) {
-      bagColors.add(curr.outerBag);
-    }
-
-    return (acc += hasGoldenBag ? 1 : 0);
+    const isGoldenBag = curr.outerBag.includes(SHINY_GOLD);
+    return (acc += hasGoldenBag && !isGoldenBag ? 1 : 0);
   }, 0);
 
-  // console.log('bagColors', bagColors, bagColors.size);
-
   success(`Part 1: ${bagsContainingGoldenBag}`);
-  success(`Part 2: ${bagAmount('shiny gold', input)}`);
+  success(`Part 2: ${calcBagAmount(SHINY_GOLD, input)}`);
 };
 
-type BagAmount = (innerBag: string, input: Array<Bag>) => number;
+type CalcBagAmount = (innerBag: string, input: Array<Bag>) => number;
 
-const bagAmount: BagAmount = (outerBag, input) => {
+const calcBagAmount: CalcBagAmount = (outerBag, input) => {
   const bag = input.find((o) => o.outerBag === outerBag);
-  // console.log('bag', bag);
 
   if (!bag?.innerBags) {
     return 0;
@@ -65,24 +52,24 @@ const bagAmount: BagAmount = (outerBag, input) => {
   let sum = 0;
 
   for (const [name, count] of Object.entries(bag.innerBags)) {
-    sum += count + count * bagAmount(name, input);
+    sum += count + count * calcBagAmount(name, input);
   }
 
   return sum;
 };
 
-type ContainsShinyGoldBag = (outerBag: string, input: Array<Bag>) => boolean;
+type ContainsGoldenBag = (outerBag: string, input: Array<Bag>) => boolean;
 
-const containsGoldenBag: ContainsShinyGoldBag = (outerBag, input) => {
-  const bags = input.find((o) => o.outerBag === outerBag);
+const containsGoldenBag: ContainsGoldenBag = (outerBag, input) => {
+  const bag = input.find((o) => o.outerBag === outerBag);
 
-  if (!bags?.innerBags) {
+  if (!bag?.innerBags) {
     return false;
   }
 
-  const { innerBags } = bags;
+  const { innerBags } = bag;
 
-  if (Object.keys(innerBags).some((o) => o.includes('shiny gold'))) {
+  if (Object.keys(innerBags).some((o) => o.includes(SHINY_GOLD))) {
     return true;
   }
 
