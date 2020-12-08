33b24cd072fbe8c6710bf8d8421aa70c319f034e
Mauricio Ludwig <mrdamiengallagher@gmail.com>
2020-12-08 07:19:19 +0100

    refactor solution

--- 2020/days/08.ts
+++ 2020/days/08.ts
@@ -1,103 +1,89 @@
 import { getAsArray } from '../input';
 import { success } from '../utils/logger';
 
+type TInstruction = {
+  operation: string;
+  num: number;
+};
+
+class Console {
+  sequence: Array<TInstruction> = [];
+  globalAcc = 0;
+  pastOperations = [0];
+  valid = true;
+
+  constructor(sequence: Array<TInstruction>) {
+    this.sequence = [...sequence.map((o) => ({ ...o }))];
+  }
+
+  run(): void {
+    for (let i = 0; i < this.sequence.length; ) {
+      const { operation, num } = this.sequence[i];
+
+      switch (operation) {
+        case 'acc':
+          this.globalAcc += num;
+          i++;
+          break;
+        case 'jmp':
+          i += num;
+          break;
+        case 'nop':
+          i++;
+          break;
+        default:
+          throw new Error(`No case matched (${operation}).`);
+      }
+
+      if (this.pastOperations.includes(i)) {
+        this.valid = false;
+        break;
+      } else {
+        this.pastOperations.push(i);
+      }
+    }
+  }
+
+  get acc(): number {
+    return this.globalAcc;
+  }
+}
+
 export default (): void => {
-  const sequence = getAsArray('08.txt').map((o) => {
+  const sequence: Array<TInstruction> = getAsArray('08.txt').map((o) => {
     const match = o.match(/(acc|jmp|nop) (\+|\-)(\d+)/);
 
     if (!match) {
       throw new Error('No match found.');
     }
 
-    const [, instruction, unaryOperator, num] = match;
+    const [, operation, unaryOperator, num] = match;
 
     return {
-      instruction,
+      operation,
       num: parseInt(num, 10) * (unaryOperator === '+' ? 1 : -1),
     };
   });
 
-  let globalAcc = 0;
-  const pastOperations = [0];
+  const console = new Console(sequence);
+  console.run();
+  success(`Part 1: ${console.acc}`);
 
-  for (let i = 0; i < sequence.length; ) {
-    const { instruction, num } = sequence[i];
+  let lastIndex = 0;
 
-    switch (instruction) {
-      case 'acc':
-        globalAcc += num;
-        i++;
-        break;
-      case 'jmp':
-        i += num;
-        break;
-      case 'nop':
-        i++;
-        break;
-      default:
-        throw new Error('No case matched.');
-    }
+  while (true) {
+    const console = new Console(sequence);
+    const nextIndex = console.sequence.findIndex(
+      (o, i) => i > lastIndex && ['jmp', 'nop'].some((x) => x === o.operation)
+    );
+    lastIndex = nextIndex;
+    console.sequence[nextIndex].operation =
+      console.sequence[nextIndex].operation === 'jmp' ? 'nop' : 'jmp';
+    console.run();
 
-    if (pastOperations.includes(i)) {
+    if (console.valid) {
+      success(`Part 2: ${console.acc}`);
       break;
-    } else {
-      pastOperations.push(i);
     }
   }
-
-  success(`Part 1: ${globalAcc}`);
-
-  (() => {
-    let lastIndex = 0;
-
-    while (true) {
-      const newSequence = [...sequence.map((o) => ({ ...o }))];
-      const nextIndex = newSequence.findIndex(
-        (o, i) =>
-          i > lastIndex && ['jmp', 'nop'].some((x) => x === o.instruction)
-      );
-      lastIndex = nextIndex;
-
-      if (newSequence[nextIndex].instruction === 'jmp') {
-        newSequence[nextIndex].instruction = 'nop';
-      } else {
-        newSequence[nextIndex].instruction = 'jmp';
-      }
-
-      let globalAcc = 0;
-      const pastOperations = [0];
-      let valid = true;
-
-      for (let i = 0; i < newSequence.length; ) {
-        const { instruction, num } = newSequence[i];
-
-        switch (instruction) {
-          case 'acc':
-            globalAcc += num;
-            i++;
-            break;
-          case 'jmp':
-            i += num;
-            break;
-          case 'nop':
-            i++;
-            break;
-          default:
-            throw new Error('No case matched.');
-        }
-
-        if (pastOperations.includes(i)) {
-          valid = false;
-          break;
-        } else {
-          pastOperations.push(i);
-        }
-      }
-
-      if (valid) {
-        success(`Part 2: ${globalAcc}`);
-        break;
-      }
-    }
-  })();
 };
