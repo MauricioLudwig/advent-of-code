7a6980f2b73bb4a463f87eae0ab8657f948c9da8
Mauricio Ludwig <mrdamiengallagher@gmail.com>
2020-12-12 01:19:47 +0100

    add utility method for printing grids

--- 2020/utils/logger.ts
+++ 2020/utils/logger.ts
@@ -8,3 +8,18 @@ export const danger = <T>(msg: T): void => console.log(chalk.red(msg));
 
 export const logPerformance = (t2: number, t1: number): void =>
   highlight(`Time to execute (in ms): ${(t2 - t1).toFixed(4)}`);
+
+export const printGrid = <T>(grid: T[][]): void => {
+  let string = chalk.magentaBright(
+    `\nGrid: ${grid.length} x ${grid[0].length}\n\n`
+  );
+
+  grid.forEach((row) => {
+    row.forEach((col) => {
+      string += col;
+    });
+    string += '\n';
+  });
+
+  console.log(string);
+};
