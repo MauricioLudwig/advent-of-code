78e1d5fa066423428fbc80c82fc5c2bf714a496c
Mauricio Ludwig <mrdamiengallagher@gmail.com>
2022-12-01 06:10:08 +0100

    day 1 (2022)

---
+++ 2022/01.ts
@@ -0,0 +1,16 @@
+import { TDayFn } from "../@@types";
+import { Divisor, Input, Logger } from "../@@utils";
+
+export default async (): TDayFn => {
+  const input = new Input("./2022/files/01.txt").AsMatrix(Divisor.NewLine);
+  const counts = input.map((group) =>
+    group.reduce((acc, curr) => acc + parseInt(curr, 10), 0)
+  );
+
+  Logger.success(`Part 1: ${Math.max(...counts)}`);
+
+  counts.sort((a, b) => b - a);
+  const topThree = counts.slice(0, 3).reduce((acc, curr) => acc + curr, 0);
+
+  Logger.success(`Part 2: ${topThree}`);
+};
