be419c144bb49f03991b2847d246ac2e9e87e09c
Mauricio Ludwig <mrdamiengallagher@gmail.com>
2023-12-05 14:01:18 +0100

    day 5, part 1 (2023)

---
+++ 2023/05.ts
@@ -0,0 +1,77 @@
+import { Input, Logger } from "../@@utils";
+
+type SeedMap = {
+  from: string;
+  to: string;
+  ranges: Array<{
+    destination: number;
+    source: number;
+    range: number;
+  }>;
+};
+
+export default async () => {
+  const [seedsLs, ...rest] = new Input("./2023/files/05.txt").AsMatrix("");
+
+  const seeds = (seedsLs?.[0]!.match(/(\d+)/g) ?? []).map(Number);
+
+  const seedMaps = rest.map((o): SeedMap => {
+    const [text, ...numbers] = o;
+    const [, from, to] = text!.match(/(\w+)-to-(\w+)/) ?? [];
+
+    const ranges = numbers.map((range) => {
+      const [d, s, r] = (range.match(/(\d+)/g) ?? []).map(Number);
+      return {
+        destination: d!,
+        source: s!,
+        range: r!,
+      };
+    });
+
+    return {
+      from: from!,
+      to: to!,
+      ranges,
+    };
+  });
+
+  Logger.success(`Part 1: ${part1(seeds, seedMaps)}`);
+  Logger.success(`Part 2: ${part2(seeds, seedMaps)}`);
+};
+
+const part2 = (seeds: number[], seedMaps: SeedMap[]) => {
+  return 0;
+};
+
+const part1 = (seeds: number[], seedMaps: SeedMap[]): number => {
+  const locationNumbers = seeds.map(
+    (o) => findDestinationRecursive("seed", o, seedMaps)[1]
+  );
+  return Math.min(...locationNumbers);
+};
+
+export const findDestinationRecursive = (
+  from: string,
+  source: number,
+  seedMaps: SeedMap[]
+): [string, number] => {
+  const seedMap = seedMaps.find((o) => o.from === from);
+
+  if (!seedMap) {
+    return [from, source];
+  }
+
+  const nextSeedMap = seedMap.ranges.find(
+    (o) => source >= o.source && source < o.range + o.source
+  );
+
+  if (!nextSeedMap) {
+    return findDestinationRecursive(seedMap.to, source, seedMaps);
+  }
+
+  const destination = nextSeedMap.destination - nextSeedMap.source + source;
+  return findDestinationRecursive(seedMap.to, destination, seedMaps);
+};
+
+export const getRange = (start: number, range: number): number[] =>
+  [...Array(range)].map((_, i) => i + start);
